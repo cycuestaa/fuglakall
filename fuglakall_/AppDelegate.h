84dc8455b2296fc375628db5f4501c290e4648be
Camila C. A <cycuesta@gmail.com>
2021-02-18 13:01:07 -0500

    Rec,Stop,Play,Show
    Prototype from presentation

---
+++ fuglakall_/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  fuglakall_
+//
+//  Created by Camila Y Cuesta Arcentales on 2/17/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ fuglakall_/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  fuglakall_
+//
+//  Created by Camila Y Cuesta Arcentales on 2/17/21.
+//
+
+#import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
+
+@interface ViewController : UIViewController <AVAudioRecorderDelegate, AVAudioPlayerDelegate>
+
+@end
