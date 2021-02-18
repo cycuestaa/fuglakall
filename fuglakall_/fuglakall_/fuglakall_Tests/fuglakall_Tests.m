28f4f29794d5821b281cd23ccbfcf835ebb0c9e9
Camila C. A <cycuesta@gmail.com>
2021-02-18 12:17:09 -0500

    record,stop,play

---
+++ fuglakall_/fuglakall_/fuglakall_Tests/fuglakall_Tests.m
@@ -0,0 +1,36 @@
+//
+//  fuglakall_Tests.m
+//  fuglakall_Tests
+//
+//  Created by Camila Y Cuesta Arcentales on 2/17/21.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface fuglakall_Tests : XCTestCase
+
+@end
+
+@implementation fuglakall_Tests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
